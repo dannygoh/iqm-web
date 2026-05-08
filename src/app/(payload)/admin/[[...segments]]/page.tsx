1ffca466c010b5a24c510da9d0a1cd13186b3900
Danny Goh <4446371+dannygoh@users.noreply.github.com>
2026-05-09 03:42:02 +0800

    fix: await getPayload before RootPage to prime React.cache singleton in admin route

--- src/app/(payload)/admin/[[...segments]]/page.tsx
+++ src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -1,15 +1,21 @@
 import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
+import { getPayload } from 'payload'
 import config from '@payload-config'
 import { importMap } from '../importMap.js'
 
-type PageProps = {
+type Args = {
   params: Promise<{ segments: string[] }>
   searchParams: Promise<{ [key: string]: string | string[] }>
 }
 
-export const generateMetadata = (props: PageProps) =>
+export const generateMetadata = ({ params, searchParams }: Args) =>
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  generatePageMetadata({ config, params: props.params } as any)
+  generatePageMetadata({ config, params, searchParams } as any)
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default (props: PageProps) => RootPage({ ...props, config, importMap } as any)
+export default async function Page({ params, searchParams }: Args) {
+  // Prime the Payload React.cache singleton before RootPage's initReq runs,
+  // preventing 'Cannot destructure config of undefined' in partialReqCache.get().
+  await getPayload({ config })
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return RootPage({ config, importMap, params, searchParams } as any)
+}
